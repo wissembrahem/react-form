ad476a082f464a178518eca30c6ab596886232c1
Wissembrahem <wbrahem19@gmail.com>
2025-12-01 17:34:34 +0100

    react from

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ function App() {
   const [travelTitles, setTravelTitles] = useState([
     "Visitare Cartagine e i suoi monumenti",
     "Scoprire il punto più a nord del continente africano",
+    "Visitare Cap Zebib",
   ]);
   const [newTravelTitle, setNewTravelTitle] = useState('');
 
   function addTravelTitle(event) {
     event.preventDefault();
+    const newTravelTitles = [...travelTitles, newTravelTitle]
+    setTravelTitles(newTravelTitles)
+    setNewTravelTitle('')
   }
 
   return (
     <>
       <main>
-        <form onSubmit={}>
+        <form onSubmit={addTravelTitle}>
           <input type="text" value={newTravelTitle} onChange={(event) => {
             setNewTravelTitle(event.target.value)
           }} />
